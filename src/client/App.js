d90e746f6d8bde91fb93111708509d54c62e10a9
Haukur Már Böðvarsson <haukurmar@gmail.com>
2018-07-17 17:03:30 +0000

    Adding react-router and one route component.

--- src/client/App.js
+++ src/client/App.js
@@ -1,3 +1,12 @@
 import React from 'react';
-const App = () => <div>Hello World!</div>
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './routes/Home'
+
+const App = () => (
+	<Router>
+		<Switch>
+			<Route exact path="/" component={Home} />
+		</Switch>
+	</Router>
+)
 export default App;
